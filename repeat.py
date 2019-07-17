e8056b13bae0e9715144a38b1a1b042013f88091
aksharyash <47146368+aksharyash@users.noreply.github.com>
2019-07-17 21:09:43 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,1 +1,17 @@
-
+n=input()
+a=input()
+l=[]
+for i in a:
+    c=0
+    for j in a:
+        if i==j:
+            c+=1
+            if c>=2:
+                l.append(j)
+                c=0
+        else:
+            pass
+print(sorted(set(l)))
+    
+    
+  
