009d409b07800ce8d08c981fa4f36fec4b14ab82
aksharyash <47146368+aksharyash@users.noreply.github.com>
2019-07-18 15:56:45 +0530

    Update twice.py

--- twice.py
+++ twice.py
@@ -1,1 +1,14 @@
-
+n=input()
+a=input().split()
+b=[]
+c=0
+for i in a:
+    c=0
+    for j in a:
+        if i==j:
+            c+=1
+        else:
+            pass
+        if c>=2:
+            b.append(i)
+print(' '.join([x for x in a if x not in b]))
