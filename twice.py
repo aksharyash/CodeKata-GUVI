374f7f15283c96ad6dc2a9832534f3c368250148
aksharyash <47146368+aksharyash@users.noreply.github.com>
2019-08-02 18:28:23 +0530

    Update twice.py

--- twice.py
+++ twice.py
@@ -9,6 +9,6 @@ for i in a:
             c+=1
         else:
             pass
-        if c>=2:
-            b.append(i)
+    if c>=2:
+        b.append(i)
 print(' '.join([x for x in a if x not in b]))
