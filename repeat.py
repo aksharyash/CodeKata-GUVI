8c08fd2dee8c92fc8d6ee6df9e3fe6d3fe1de9a0
aksharyash <47146368+aksharyash@users.noreply.github.com>
2019-08-06 07:38:12 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,12 +1,17 @@
 n=int(input())
 inp=list(input().split())
 str=""
+lst=[]
 for i in inp:
     if i in str:
-        print(i)
+        lst.append(i)
         break
     else:
         str=str+i
+if lst==[]:
+    print("unique")
+else:
+    print(''.join(lst))
     
     
   
