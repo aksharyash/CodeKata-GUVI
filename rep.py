fa26422bf4310df4f5c5f761bde30789897f7639
aksharyash <47146368+aksharyash@users.noreply.github.com>
2019-08-06 07:41:38 +0530

    Update rep.py

--- rep.py
+++ rep.py
@@ -1,1 +1,14 @@
-
+n=int(input())
+inp=list(input().split())
+str=""
+lst=[]
+for i in inp:
+    if i in str:
+        lst.append(i)
+        break
+    else:
+        str=str+i
+if lst==[]:
+    print("unique")
+else:
+    print(''.join(lst))
