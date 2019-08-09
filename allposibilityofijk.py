81c49927e4e165ce3579dce662b11d62074fd949
aksharyash <47146368+aksharyash@users.noreply.github.com>
2019-08-09 23:53:17 +0530

    Update allposibilityofijk.py

--- allposibilityofijk.py
+++ allposibilityofijk.py
@@ -1,6 +1,6 @@
 n=int(input())
 inp=list(map(int,input().split()))
-lst=[[inp[i],inp[j],inp[k]] for i in range(len(inp)) for j in range(len(inp)) for k in range(len(inp))if inp[i]+inp[j]==inp[k] and inp[i]<inp[j]<inp[k]]
+lst=[[inp[i],inp[j],inp[k]] for i in range(len(inp)) for j in range(len(inp)) for k in range(len(inp))if inp[i]+inp[j]==inp[k] and i<j<k]
 for i in lst:
     for j in i:
         if j=='[' or j==']' or j==',':
